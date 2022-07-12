86cc0b439fd2f7d254e18ebae48eefd8a490bf3e
Inaldo Teotonio <inaldo.teotonio@outlook.com>
2022-07-12 20:27:59 -0300

    First component

---
+++ frontend/src/componets/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from "..//..//Assets/img/Notification-icon.svg"
+import "./styles.css"
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src= {icon} alt="Notification" />
+        </div>
+    )
+}
+
+export default NotificationButton
